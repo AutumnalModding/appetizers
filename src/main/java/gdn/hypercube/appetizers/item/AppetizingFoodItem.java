c6eb56d9e38782b2026d0534dae1e851c4c7f645
Lily Autumn <realprismaticyt@gmail.com>
2026-05-03 14:57:38 +1200

    kill blockitem

--- src/main/java/gdn/hypercube/appetizers/item/AppetizingFoodItem.java
+++ src/main/java/gdn/hypercube/appetizers/item/AppetizingFoodItem.java
@@ -1,40 +1,41 @@
 package gdn.hypercube.appetizers.item;
 
-import gdn.hypercube.appetizers.AppetizersInit;
-import java.lang.reflect.Field;
+//import gdn.hypercube.appetizers.AppetizersInit;
+//import java.lang.reflect.Field;
 import java.util.function.Consumer;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+//import net.minecraft.block.AbstractBlock;
+//import net.minecraft.block.Block;
+//import net.minecraft.block.BlockState;
+//import net.minecraft.block.Blocks;
 import net.minecraft.component.type.ConsumableComponent;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.component.type.TooltipDisplayComponent;
-import net.minecraft.item.BlockItem;
+//import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemPlacementContext;
+//import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
+//import net.minecraft.item.ItemUsageContext;
+//import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.item.consume.ConsumeEffect;
 import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+//import net.minecraft.registry.Registries;
+//import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
+//import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
-public class AppetizingFoodItem extends BlockItem {
-    private final String name;
+// TODO: blockitem impl
+public class AppetizingFoodItem extends Item {
+//    private final String name;
     private final String tooltip;
 
     public AppetizingFoodItem(String name, String tooltip, boolean always, int hunger, int saturation, RegistryEntry<SoundEvent> sound, float time, ConsumeEffect... effects) {
-        this.name = name;
+//        this.name = name;
 
         FoodComponent.Builder food = new FoodComponent.Builder();
         if (always) food = food.alwaysEdible();
@@ -53,53 +54,53 @@ public class AppetizingFoodItem extends BlockItem {
                 .food(food.build(), consumable.build());
 
         this.tooltip = tooltip;
-        super(null, settings);
+        super(settings);
     }
 
-    public AppetizingFoodItem setBlock(Block target) {
-        try {
-            Field field = BlockItem.class.getDeclaredField("block");
-            field.setAccessible(true);
-            field.set(this, target);
-        } catch (ReflectiveOperationException exception) {
-            AppetizersInit.LOGGER.error("Failed to set block of item {} to {}!", this.name, target.getName().getLiteralString());
-            AppetizersInit.LOGGER.error("Dumping stacktrace!", exception);
-        }
-
-        return this;
-    }
-
-    public AppetizingFoodItem registerBlock(Block copyTarget) {
-        Block target = new Block(AbstractBlock.Settings.copy(copyTarget).breakInstantly().registryKey(RegistryKey.of(
-                RegistryKeys.BLOCK,
-                Identifier.of("appetizers", this.name))
-        ));
-
-        Registry.register(Registries.BLOCK, Identifier.of("appetizers", this.name), target);
-        this.setBlock(target);
-
-        return this;
-    }
-
-   @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        return this.getBlock() == null ? ActionResult.PASS : super.useOnBlock(context);
-    }
-
-    @Override
-    public ActionResult place(ItemPlacementContext context) {
-        return this.getBlock() == null ? ActionResult.PASS : super.place(context);
-    }
-
-    @Override
-    protected boolean place(ItemPlacementContext context, BlockState state) {
-        return this.getBlock() != null && super.place(context, state);
-    }
-
-    @Override
-    protected boolean canPlace(ItemPlacementContext context, BlockState state) {
-        return this.getBlock() != null && super.canPlace(context, state);
-    }
+//    public AppetizingFoodItem setBlock(Block target) {
+//        try {
+//            Field field = BlockItem.class.getDeclaredField("block");
+//            field.setAccessible(true);
+//            field.set(this, target);
+//        } catch (ReflectiveOperationException exception) {
+//            AppetizersInit.LOGGER.error("Failed to set block of item {} to {}!", this.name, target.getName().getLiteralString());
+//            AppetizersInit.LOGGER.error("Dumping stacktrace!", exception);
+//        }
+//
+//        return this;
+//    }
+//
+//    public AppetizingFoodItem registerBlock(Block copyTarget) {
+//        Block target = new Block(AbstractBlock.Settings.copy(copyTarget).breakInstantly().registryKey(RegistryKey.of(
+//                RegistryKeys.BLOCK,
+//                Identifier.of("appetizers", this.name))
+//        ));
+//
+//        Registry.register(Registries.BLOCK, Identifier.of("appetizers", this.name), target);
+//        this.setBlock(target);
+//
+//        return this;
+//    }
+//
+//   @Override
+//    public ActionResult useOnBlock(ItemUsageContext context) {
+//        return this.getBlock() == null ? ActionResult.PASS : super.useOnBlock(context);
+//    }
+//
+//    @Override
+//    public ActionResult place(ItemPlacementContext context) {
+//        return this.getBlock() == null ? ActionResult.PASS : super.place(context);
+//    }
+//
+//    @Override
+//    protected boolean place(ItemPlacementContext context, BlockState state) {
+//        return this.getBlock() != null && super.place(context, state);
+//    }
+//
+//    @Override
+//    protected boolean canPlace(ItemPlacementContext context, BlockState state) {
+//        return this.getBlock() != null && super.canPlace(context, state);
+//    }
 
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, TooltipDisplayComponent component, Consumer<Text> consumer, TooltipType type) {
